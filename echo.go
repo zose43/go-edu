3091c7d08ca61f69ea740db0a7f6f9379e3baa42
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-02-26 21:44:55 +0300

    update

---
+++ echo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	echoFromLoop()
+}
+
+func echoFromLoop()  {
+	for i, arg := range os.Args[1:] {
+		fmt.Println(arg + "-" + strconv.Itoa(i))
+	}
+}
+
+func echoAlternative()  {
+	fmt.Println(strings.Join(os.Args[1:]," "))
+}
