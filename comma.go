22c5554fcf3f9884aa9dc75bdd14d8c1a8afa55e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-03-17 17:46:04 +0300

    update comma

--- comma.go
+++ comma.go
@@ -1,8 +1,12 @@
 package main
 
+import (
+	"bytes"
+)
+
 func main() {
-	s := "1332543"
-	println(comma(s))
+	s := "133555223"
+	println(comma2(s))
 }
 
 func comma(s string) string {
@@ -11,4 +15,19 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func comma2(s string) string {
+	var buf bytes.Buffer
+	l := len(s)
+	if l <= 3 {
+		return s
+	}
+	for i := 0; i <= l-1; i++ {
+		if (l-i)%3 == 0 && i != 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteByte(s[i])
+	}
+	return buf.String()
 }
