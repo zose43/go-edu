1debcfb0ebd56bd65a45444124efad6993c2576b
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-03-18 12:52:25 +0300

    upd bin

--- bin.go
+++ bin.go
@@ -8,11 +8,11 @@ var y = 1<<1 | 1<<2
 func main() {
 	fmt.Printf("{1,5} %08b\n", x)
 	fmt.Printf("{1,2} %08b\n", y)
-	fmt.Printf("{1} %08b\n", x&y)     // intersection
-	fmt.Printf("{1,2,5} %08b\n", x|y) // union, addiction if int = degree of 2
-	fmt.Printf("{5,2} %08b\n", x^y)   // symmetric subtraction
-	fmt.Printf("{5} %08b\n", x&^y)    // subtraction
-	fmt.Printf("{5} %08b\n", ^x)      // inversion
+	fmt.Printf("intersection, {1} %08b\n", x&y)            // intersection
+	fmt.Printf("union, {1,2,5} %08b\n", x|y)               // union, addiction if int = degree of 2
+	fmt.Printf("symmetric subtraction, {5,2} %08b\n", x^y) // symmetric subtraction
+	fmt.Printf("subtraction,{5} %08b\n", x&^y)             // subtraction
+	fmt.Printf("inversion, {5} %08b\n", ^x)                // inversion
 	fmt.Printf("{3,7} %08b\n", x<<2)
 	fmt.Printf("{0,4} %08b\n", x>>1)
 	for i := uint(0); i < 8; i++ {
