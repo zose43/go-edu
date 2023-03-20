3ade51c0145581becbde75bce27238208ffd8e1e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-03-20 22:53:06 +0300

    upd append

--- append.go
+++ append.go
@@ -5,33 +5,33 @@ import (
 )
 
 func main() {
-var y,x []int
-	for i:=0; i<= 10;i++ {
-		if i&1 !=0 {
-			y = appendInt(y,i,i*i)
+	var y, x []int
+	for i := 0; i <= 10; i++ {
+		if i&1 != 0 {
+			y = appendInt(y, i, i*i)
 		} else {
-			y = appendInt(y,i)
+			y = appendInt(y, i)
 		}
-		fmt.Printf("%d cap=%d\t%v\n",i,cap(y),y)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
 	}
-	x = append(x,2007,2006)
-	fmt.Printf("len=%d cap=%d\t%v\n",len(x),cap(x),x)
+	x = append(x, 2007, 2006)
+	fmt.Printf("len=%d cap=%d\t%v\n", len(x), cap(x), x)
 	bd := []int{21, 12, 1995}
 	x = append(x, bd...)
-	fmt.Printf("len=%d cap=%d\t%v\n",len(x),cap(x),x)
+	fmt.Printf("len=%d cap=%d\t%v\n", len(x), cap(x), x)
 }
 
-func appendInt(s []int, x ...int)[]int  {
+func appendInt(s []int, x ...int) []int {
 	var z []int
-	zlen:= len(s)+ len(x)
+	zlen := len(s) + len(x)
 	if zlen <= cap(s) {
 		z = s[:zlen]
 	} else {
-		zcap:= zlen
+		zcap := zlen
 		if zcap < len(s)*2 {
-			zcap = len(s)*2
+			zcap = len(s) * 2
 		}
-		z = make([]int,zlen,zcap)
+		z = make([]int, zlen, zcap)
 		copy(z, s)
 	}
 	copy(z[len(s):], x)
