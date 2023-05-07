c66dbe0cc434ece0b2575ab71002b33c37272a54
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-05-07 10:44:24 +0300

    add custom flag to tempconv

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -2,11 +2,17 @@ package tempconv
 
 // Package tempConv performs Fahrenheits and Celsius conversions
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 type Celsius float32
 type Fahrenheit float32
 type Kelvin float32
+type celsiusFlag struct {
+	Celsius
+}
 
 const (
 	AbsoluteZeroC Celsius    = -273.15
@@ -19,6 +25,30 @@ const (
 	BoilingK      Kelvin     = 373.2
 	BoilingF      Fahrenheit = 212
 )
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "°C", "C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "°F", "F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K":
+		f.Celsius = KToC(Kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("Invalid temp %q\n", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
 
 func (c Celsius) String() string {
 	return fmt.Sprintf("%f°C", c)
