dccd9f759f28f97f05ec5a94aa167b4da6c8b53e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-05-11 21:55:06 +0300

    crud

--- http/handler.go
+++ http/handler.go
@@ -3,11 +3,68 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type dollars float32
 
 type Database map[string]dollars
+
+func (d Database) Update(w http.ResponseWriter, r *http.Request) {
+	qvalues := r.URL.Query()
+	if !qvalues.Has("i") || !qvalues.Has("p") {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Can't create item, empty params\n")
+		return
+	}
+
+	item := qvalues.Get("i")
+	price, err := strconv.ParseFloat(qvalues.Get("p"), 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Can't create item, invalid price %q\n", qvalues.Get("p"))
+		return
+	}
+	if _, ok := d[item]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Can't create item,%q not exist\n", item)
+		return
+	}
+
+	d[item] = dollars(price)
+	http.Redirect(w, r, fmt.Sprintf("/show?i=%s", item), 302)
+}
+
+func (d Database) Delete(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("i")
+	delete(d, item)
+	http.Redirect(w, r, "/list", 302)
+}
+
+func (d Database) Create(w http.ResponseWriter, r *http.Request) {
+	qvalues := r.URL.Query()
+	if !qvalues.Has("i") || !qvalues.Has("p") {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Can't create item, empty params\n")
+		return
+	}
+
+	item := qvalues.Get("i")
+	price, err := strconv.ParseFloat(qvalues.Get("p"), 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Can't create item, invalid price %q\n", qvalues.Get("p"))
+		return
+	}
+	if _, ok := d[item]; ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Can't create item, double %q\n", item)
+		return
+	}
+
+	d[item] = dollars(price)
+	http.Redirect(w, r, fmt.Sprintf("/show?i=%s", item), 302)
+}
 
 func (d Database) Show(w http.ResponseWriter, r *http.Request) {
 	item := r.URL.Query().Get("i")
