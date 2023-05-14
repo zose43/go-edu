2873a76ce27f2a18135909dcb6b7a42970951d15
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-05-14 18:16:43 +0300

    add test

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -1,27 +1,34 @@
 package eval
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
 
 func TestVar_Eval(t *testing.T) {
-	// todo no parse method
 	tests := []struct {
 		name string
-		v    Var
+		expr string
 		env  Env
-		want float64
+		want string
 	}{
 		{"Sqrt",
 			"sqrt(a / pi)",
 			Env{"a": 87616, "pi": math.Pi},
-			167},
+			"167"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.v.Eval(tt.env); got != tt.want {
-				t.Errorf("Eval() = %v, want %v", got, tt.want)
+			expr, err := Parse(tt.expr)
+			if err != nil {
+				t.Errorf("Parse invalid %s", err)
+			} else {
+				fmt.Printf("\t%v => %s\n", tt.env, tt.want)
+				got := fmt.Sprintf("%.6g", expr.Eval(tt.env))
+				if got != tt.want {
+					t.Errorf("Eval() = %v, want %v", got, tt.want)
+				}
 			}
 		})
 	}
