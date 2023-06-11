7eb1587491fa467bf587462c7d38239e22c4e3bc
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-06-11 15:11:21 +0300

    upd ftp server

--- ftpserver/ftpserver.go
+++ ftpserver/ftpserver.go
@@ -6,9 +6,12 @@ import (
 	"io"
 	"log"
 	"net"
-	"os/exec"
+	"os"
 	"strings"
+	"time"
 )
+
+const FtpAbsolutePath = "/home/zose/go_projects/go-edu/files/ftp"
 
 func FtpServer(port int) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -21,54 +24,149 @@ func FtpServer(port int) {
 			log.Print(err)
 			continue
 		}
-		fmt.Fprintln(conn, "Welcome to best FTP ever!")
+		printToClient("Welcome to best FTP ever!", conn)
 		go handleCommands(conn)
 	}
 }
 
 func handleCommands(conn net.Conn) {
-	defer func() { _ = conn.Close() }()
+	defer func() { _ = conn.(*net.TCPConn).CloseWrite() }()
 	reader := bufio.NewReader(conn)
 
 	for {
 		cmdLine, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Can't read command %s", err)
+			printToClient("Can't read command, please try later", conn)
 			return
 		}
 
-		cmd, arg, exist := strings.Cut(strings.TrimSpace(cmdLine), " ")
-		if !exist && len(cmd) < 1 {
-			log.Printf("Can't parse command %s", cmdLine)
-			continue
+		cmdArgs := strings.Fields(strings.TrimSpace(cmdLine))
+		if len(cmdArgs) == 0 {
+			printToClient("Empty request", conn)
+			return
 		}
 
-		switch cmd {
-		case "ls":
-			if len(arg) == 0 {
-				arg = "."
-			}
-			execute(cmd, conn, arg)
+		arg := ""
+		if len(cmdArgs) > 1 {
+			arg = cmdArgs[1]
+		}
+
+		switch cmdArgs[0] {
+		case "list":
+			list(arg, conn)
 		case "cd":
-			// todo realize
-			//execute("bash", conn, "-c", cmdLine)
+			changeDir(arg, conn)
 		case "get":
-			execute("cat", conn, arg)
+			get(arg, conn)
 		case "close":
-			fmt.Fprintln(conn, "Bye-bye")
+			printToClient("Bye-bye", conn)
 			_ = conn.Close()
 		default:
-			fmt.Fprintf(conn, "unknown command %s\n", cmd)
+			printToClient(fmt.Sprintf("Unknown command %q", cmdArgs[0]), conn)
 		}
 	}
 }
 
-func execute(cmdLine string, conn io.Writer, arg ...string) {
-	cmd := exec.Command(cmdLine, arg...)
-	res, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Print(err)
-		fmt.Fprintf(conn, "failure execute %s, please try later\n", cmd)
+func path(relative string) (*os.File, error) {
+	dir, _ := os.Getwd()
+	openDir := fmt.Sprintf("files/ftp/%s", relative)
+	if strings.Contains(dir, "/ftp") {
+		openDir = fmt.Sprintf("%s/%s", dir, relative)
 	}
-	_, _ = conn.Write(res)
+	if relative == FtpAbsolutePath {
+		openDir = relative
+	}
+
+	openDir = strings.TrimRight(openDir, "/")
+	f, err := os.Open(openDir)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func list(arg string, conn net.Conn) {
+	msg := ""
+	f, err := path(arg)
+	if err != nil {
+		msg = fmt.Sprintf("Can't open path %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+
+	files, err := f.Readdir(0)
+	if err != nil {
+		msg = fmt.Sprintf("Can't read directory %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+
+	for _, f := range files {
+		fType := "file"
+		if f.IsDir() {
+			fType = "dir"
+		}
+		_, _ = fmt.Fprintf(conn, "%s %s %d %s %s\n",
+			f.Mode().Perm(),
+			fType,
+			f.Size(),
+			f.ModTime().Format(time.DateTime),
+			f.Name())
+	}
+}
+
+func changeDir(arg string, conn net.Conn) {
+	msg := ""
+	if len(arg) == 0 {
+		arg = FtpAbsolutePath
+	}
+	f, err := path(arg)
+	if err != nil {
+		msg = fmt.Sprintf("Can't open dir %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+	if err := f.Chdir(); err != nil {
+		msg = fmt.Sprintf("Can't change dir %q", arg)
+		printToClient(msg, conn)
+	}
+}
+
+func get(arg string, conn net.Conn) {
+	msg := ""
+	if len(arg) == 0 {
+		printToClient("Filename is empty", conn)
+		return
+	}
+
+	f, err := path(arg)
+	if err != nil {
+		msg = fmt.Sprintf("Can't open file %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+
+	info, err := f.Stat()
+	if err != nil {
+		msg = fmt.Sprintf("Can't read file %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+	if info.IsDir() {
+		printToClient("Read only from dir", conn)
+		return
+	}
+
+	text, err := io.ReadAll(f)
+	if err != nil {
+		msg = fmt.Sprintf("Can't read file %q", arg)
+		printToClient(msg, conn)
+		return
+	}
+	printToClient(string(text), conn)
+}
+
+func printToClient(msg string, conn net.Conn) {
+	_, _ = fmt.Fprintln(conn, msg)
 }
