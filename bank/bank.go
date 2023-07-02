a935b732d4e79828d2fdfa4cbc71d4eafdcdb07e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-07-02 12:30:55 +0300

    add withdraw

--- bank/bank.go
+++ bank/bank.go
@@ -1,9 +1,15 @@
 package bank
 
 var (
-	deposit = make(chan int, 10)
-	balance = make(chan int, 10)
+	deposit = make(chan int)
+	balance = make(chan int)
+	payoff  = make(chan *withdraw)
 )
+
+type withdraw struct {
+	amount int
+	ch     chan<- bool
+}
 
 func Deposit(amount int) {
 	deposit <- amount
@@ -13,12 +19,26 @@ func Balance() int {
 	return <-balance
 }
 
+func WithDraw(amount int) bool {
+	ch := make(chan bool)
+	wd := withdraw{ch: ch, amount: amount}
+	payoff <- &wd
+	return <-ch
+}
+
 func teller() {
 	var balances int
 	for {
 		select {
 		case amount := <-deposit:
 			balances += amount
+		case wd := <-payoff:
+			if balances >= wd.amount {
+				balances -= wd.amount
+				wd.ch <- true
+			} else {
+				wd.ch <- false
+			}
 		case balance <- balances:
 		}
 	}
